b287a31f8daf3649eecbf9637127584a5e6f62ef
David Hellsing <david@aino.se>
2014-06-27 21:40:45 +0200

    Building from version v1.0.6

--- dist/finger.js
+++ dist/finger.js
@@ -1,5 +1,6 @@
-var detect = require('ainojs-detect')
-var requestFrame = require('ainojs-requestframe')
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Finger=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+var detect = _dereq_('ainojs-detect')
+var requestFrame = _dereq_('ainojs-requestframe')
 
 // shortcuts
 var document = window.document,
@@ -15,7 +16,6 @@ var unbind = function(elem, type, handler) {
   elem.removeEventListener(type, handler, false)
 }
 
-// track velocity
 var tracker = []
 
 var getWidth = function(elem) {
@@ -266,3 +266,97 @@ Finger.prototype = {
 }
 
 module.exports = Finger
+},{"ainojs-detect":2,"ainojs-requestframe":3}],2:[function(_dereq_,module,exports){
+/*
+ * Detect
+ * Detects various browser features and vendors
+ */
+
+var ua = navigator.userAgent.toLowerCase()
+var isWebkit = ua.indexOf( "applewebkit" ) > -1
+var platform = navigator.platform
+
+module.exports = {
+
+  touch: !!('ontouchstart' in document),
+
+  canvas: !!( 'getContext' in document.createElement('canvas') ),
+
+  svg: (function() {
+    return !! document.createElementNS &&
+           !! document.createElementNS('http://www.w3.org/2000/svg','svg').createSVGRect
+  }()),
+
+  ie: (function() {
+
+    var v = 3
+    var div = document.createElement( 'div' )
+    var all = div.getElementsByTagName( 'i' )
+
+    do
+      div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->'
+    while 
+      (all[0])
+    return v > 4 ? v : document.documentMode
+
+  }()),
+
+  iphone: isWebkit && /iPhone/.test( platform ),
+  ipad:   isWebkit && /iPad/.test( platform ),
+  ipod:   isWebkit && /iPod/.test( platform ),
+  ios:    isWebkit && /iPhone|iPad|iPod/.test( platform ),
+  safari: !!(/safari/.test(ua) && !(/chrome/.test(ua))),
+  chrome: /chrome/.test(ua),
+
+  mac: platform.toUpperCase().indexOf('MAC') >= 0,
+
+  retina: (function() {
+    var dp = window.devicePixelRatio
+    var mm = window.matchMedia
+    if ( mm ) {
+      var mq = mm("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)")
+      return !!( (mq && mq.matches) || dp > 1 )
+    } 
+    return dp ? dp > 1 : false
+  }()),
+  
+  translate3d: (function() {
+    var el = document.createElement('p')
+    var has3d
+    var transforms = {
+        'webkitTransform':'-webkit-transform',
+        'OTransform':'-o-transform',
+        'msTransform':'-ms-transform',
+        'MozTransform':'-moz-transform',
+        'transform':'transform'
+    }
+
+    document.documentElement.insertBefore(el, null)
+
+    for (var t in transforms) {
+      if ( typeof el.style[t] != 'undefined' ) {
+        el.style[t] = 'translate3d(1px,1px,1px)'
+        has3d = window.getComputedStyle(el).getPropertyValue(transforms[t])
+      }
+    }
+
+    document.documentElement.removeChild(el)
+    return (has3d !== undefined && has3d.length > 0 && has3d !== "none")
+  }())
+}
+
+},{}],3:[function(_dereq_,module,exports){
+module.exports = (function(){
+  var r = 'RequestAnimationFrame'
+  return window.requestAnimationFrame || 
+    window['webkit'+r] || 
+    window['moz'+r] || 
+    window['o'+r] || 
+    window['ms'+r] || 
+    function( callback ) {
+      window.setTimeout(callback, 1000 / 60)
+    }
+}())
+},{}]},{},[1])
+(1)
+});
